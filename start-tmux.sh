0ecdb5f4df3f4cb218b20ea63176d235f6765727
Martin Krizek <mkrizek@redhat.com>
2017-08-14 14:33:09 +0200

    Update tmux.conf

--- start-tmux.sh
+++ start-tmux.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 tmux new-session -d -s work
 
-tmux new-window -t work:1 -n 'sys'
-tmux split-window -t work:1 -h
-tmux new-window -t work:2 -n 'dev'
-tmux split-window -t work:2 -h
-tmux new-window -t work:3 -n 'irc'
+tmux rename-window -t work:0 'sys'
+tmux new-window -t work:1 -n 'vim'
 
-tmux select-window -t work:1
+tmux select-window -t work:0
 tmux -2 attach-session -t work
